d853135daff5ccba3cea13fb9d1e9804609dd4e6
nancyzj <ancizj@hotmail.com>
2017-12-13 21:33:17 -0600

    obtiene y crea las listas de los libros

--- js/app.js
+++ js/app.js
@@ -1,7 +1,26 @@
 (function() {
+	var bookList = $('#book-list');
 	function loadPage() {
-		alert('hola');
-	}
+		getDataBooks();
+	};
 
+	function getDataBooks() {
+		$.getJSON( 'http://www.etnassoft.com/api/v1/get/?category=programacion&callback=?', function ( listBook ) {
+			var list = listBook;
+			for(var i=0; i< list.length; i++){
+				var item = $("<li class='item'>");
+				var h5 = $("<h5/>");
+				var p = $("<p/>");
+				var link = $("<a>")
+				link.attr('href',i);
+				h5.append('Titulo: ' + list[i].title);
+				p.append('Autor: ' + list[i].author);
+				link.append(h5);
+				link.append(p);
+				item.append(link);
+				bookList.append(item);
+			};
+		} );
+	};
 	$(document).ready(loadPage);
 })();
