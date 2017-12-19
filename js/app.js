879de8cd3380a15366bc30bd33233b6a21704d69
Lucero07 <ing.lucero.ramos@gmail.com>
2017-12-18 23:32:15 -0600

    url y title of all books

--- js/app.js
+++ js/app.js
@@ -5,11 +5,13 @@
     };
 	const getData = ()=> {
 		$.getJSON('http://www.etnassoft.com/api/v1/get/?get_categories=all&callback=?', (data)=> {
-			//console.log(data);
+			const allCategories = [];
+			let item;
 			const categories = $("#categories");
 			for (let i = 0; i < data.length; i++) {
 				const category = data[i].nicename;
-				const item = $(`<li class='item'>
+				allCategories.push(category);
+				 item = $(`<li class='item'id=item${i}>
 				<div class="collapsible-header">
 					<a> ${data[i].name} </a>
 				</div>
@@ -18,35 +20,61 @@
 				</div>
 				</li>`);
 				categories.append(item)
-				console.log('hola',data[i].name);
 			}
-
+			
+			getDataBooksCategory(allCategories)
 		})
-		 getDataBooksP();
+		
+		
+		getDataBooksP();
 		getDataBooksMB();
 		getDataBooksBD();
 	}
+	
+		$("collapsible").on("click",".item", ()=>{
+				console.log(this)
+		})
+
+	const getDataBooksCategory = (allCategories)=> {
+		const urlBooksSpecific=[];
+		for (let i = 0; i < allCategories.length; i++) {
+			let urlBookSpecific = `http://www.etnassoft.com/api/v1/get/?category=${allCategories[i]}&callback=?`;
+			urlBooksSpecific.push(urlBookSpecific);
+		}
+		getDataBooksP(urlBooksSpecific) ;
+	}
 
 
-    const getDataBooksP= ()=> {
-        $.getJSON('http://www.etnassoft.com/api/v1/get/?category=programacion&callback=?', (listBook)=> {
-            const list = listBook.length
-			const bookList = $('#book-list-P');
-            for (let i = 0; i < list; i++) {
-                const item = $("<li class='item'>");
-                const h5 = $("<h5/>");
-                const p = $("<p/>");
-                const link = $("<a>")
-                link.attr('href', i);
-                h5.append(`Titulo: ${listBook[i].title}`);
-                p.append(`Autor: ${listBook[i].author}`);
-                link.append(h5);
-                link.append(p);
-                item.append(link);
-                bookList.append(item);
-            };
+
+    const getDataBooksP= (urlBooksSpecific)=> {
+    	jQuery.map( urlBooksSpecific, function( urlBookSpecific, i ) {
+			$.getJSON(urlBookSpecific, (listBook)=> {
+				jQuery.map(listBook, (book)=> {
+					//console.log(book.title, i)
+				})
+            
+
+           
         });
+
+				});
+        
     };
+
+//const list = listBook.length;
+     // for (let i = 0; i < list; i++) {
+     //            const item = $("<li class='item'>");
+     //            const h5 = $("<h5/>");
+     //            const p = $("<p/>");
+     //            const link = $("<a>")
+     //            link.attr('href', i);
+     //            h5.append(`Titulo: ${listBook[i].title}`);
+     //            p.append(`Autor: ${listBook[i].author}`);
+     //            link.append(h5);
+     //            link.append(p);
+     //            item.append(link);
+ 
+     //        };
 	const getDataBooksBD= ()=> {
 		$.getJSON('http://www.etnassoft.com/api/v1/get/?category=bases_de_datos&callback=?', (listBook)=> {
 		//	console.log('aqui',listBook)
